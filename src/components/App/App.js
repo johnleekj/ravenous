2a71667eaf9899319e7b43119cdade9512d3c23b
johnleekj <johnleekaijie@gmail.com>
2020-05-11 16:37:19 +0800

    first commit

---
+++ src/components/App/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import SearchBar from '../SearchBar/SearchBar';
+import BusinessList from '../BusinessList/BusinessList';
+// import Business from '../components/Business';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar />
+        <BusinessList />
+      </div>
+    );
+  }
+}
+
+export default App;
